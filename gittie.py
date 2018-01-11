485da110a5bb498a53f6ce62c39a65da2a3a4b48
Gyorfisti <gyorfisti@gmail.com>
2018-01-11 09:45:13 +0100

    added shout function

--- gittie.py
+++ gittie.py
@@ -1,13 +1,12 @@
 
 def introduce():
-<<<<<<< HEAD
-    print("Hello, I'm Gittie! I am cool")
-=======
     print("Hello, I'm Gittie! This will cause a git conflict.")
->>>>>>> 326b1562b26a77022e12e7ef18ff74e14dba7f76
 
 
 def add():
     print("I will add")
 def joke():
     print("Joked!")
+
+def shout():
+    print("AAAAAAAAAAAAAAAAAAAAA")
