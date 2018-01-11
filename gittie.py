326b1562b26a77022e12e7ef18ff74e14dba7f76
Gyorfisti <gyorfisti@gmail.com>
2018-01-11 09:40:04 +0100

    modified introduce function

--- gittie.py
+++ gittie.py
@@ -1,12 +1,9 @@
 
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Hello, I'm Gittie! This will cause a git conflict.")
 
-<<<<<<< HEAD
 
 def add():
     print("I will add")
-=======
 def joke():
     print("Joked!")
->>>>>>> 9ca1af09ff246e5db40aa285a008fe06c9a2f71b
